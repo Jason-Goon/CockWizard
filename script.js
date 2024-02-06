79910647431c2583ccec7464ee8d38e31002b03a
Jason-Goon <156106640+Jason-Goon@users.noreply.github.com>
2024-02-07 00:23:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('sendButton').addEventListener('click', function() {
 
     if (userInput) {
         addToChatbox('You: ' + userInput);
-        fetch('https://api.cockwizard.com/', { // Replace with your Cloudflare Worker URL
+        fetch('https://hello-world-shrill-night-d9f1.gentoogoon.workers.dev/', { // Replace with your Cloudflare Worker URL
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
